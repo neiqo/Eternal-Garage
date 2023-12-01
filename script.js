99560e00f2902bff8c1e84d7b26d932fe8ccaf33
neil <zaquadq@gmail.com>
2023-12-01 20:53:24 +0800

    Added code to save reach out form to local storage

--- script.js
+++ script.js
@@ -41,32 +41,79 @@ function changeImage(projectId, imageSource) {
   largeImage.innerHTML = `<img src="${imageSource}" alt="Large Image">`;
 }
 
-// footer email validation
+// footer newsletter email validation
 function validateEmail() {
   const emailInput = document.getElementById("newsletteremail");
   const emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  if (emailFormat.test(emailInput.value)) {
-    // Valid email format
+  if (!emailInput.value.trim()) {
+    alert("Please enter your email address.");
+  } else if (emailFormat.test(emailInput.value)) {
     alert("Subscription Successful!");
+
+    localStorage.setItem("newsletterEmail", emailInput.value);
   } else {
-    // Invalid email format
     alert("Please enter a valid email address.");
   }
 }
 
-//loading
+// loading screen
 
 document.addEventListener("DOMContentLoaded", function () {
   const loadingScreen = document.getElementById("loading-screen");
 
-  // Add a delay to the fadeout animation
   setTimeout(function () {
     loadingScreen.classList.add("fadeout");
-  }, 500); // You can adjust the delay time (in milliseconds) as needed
+  }, 500);
 
-  // Hide the loading screen after the animation completes
   loadingScreen.addEventListener("animationend", function () {
     loadingScreen.style.display = "none";
   });
 });
+
+// contact us form
+function submitForm() {
+  // Get form values
+  const fullName = document.getElementById("full-name").value;
+  const email = document.getElementById("email").value;
+  const subject = document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
+
+  // Basic validation to check if any field is empty
+  if (!fullName || !email || !subject || !message) {
+    alert("Please fill out all sections of the form.");
+    return;
+  }
+
+  // Email format validation using a regular expression
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    alert("Please enter a valid email address.");
+    return;
+  }
+
+  // Create an object to represent the form submission
+  const formSubmission = {
+    fullName,
+    email,
+    subject,
+    message,
+    timestamp: new Date().toISOString(),
+  };
+
+  // Retrieve existing form submissions from local storage
+  const formSubmissions =
+    JSON.parse(localStorage.getItem("formSubmissions")) || [];
+
+  // Add the new submission to the array
+  formSubmissions.push(formSubmission);
+
+  // Save the updated array back to local storage
+  localStorage.setItem("formSubmissions", JSON.stringify(formSubmissions));
+
+  // Clear the form fields after successful submission
+  document.getElementById("contact-form").reset();
+
+  // Display a success message or perform other actions as needed
+  alert("Form submitted successfully!");
+}
